8abfd64a036ecd2d7d52f073b0f34a52c3eed801
syaafiudinm <altafpasallo12@gmail.com>
2024-09-29 02:16:19 +0800

    show data using resource collection

--- app/Http/Controllers/API/v2/ArticleController.php
+++ app/Http/Controllers/API/v2/ArticleController.php
@@ -2,10 +2,45 @@
 
 namespace App\Http\Controllers\API\v2;
 
-use App\Http\Controllers\Controller;
+use App\Models\Article;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Http\Controllers\Controller;
 
 class ArticleController extends Controller
 {
-    //
+
+    public function index(Request $request)
+    {
+
+        $query = Article::query()->latest('published_date');
+
+        $keyword = $request->input('title');
+
+        if($keyword) {
+            $query->where('title', 'like', '%'.$keyword.'%'); 
+        }  
+
+        $articles = $query->paginate(3);
+
+        if($articles->isEmpty()) {
+            return response()->json([
+                'data' => Response::HTTP_NOT_FOUND,
+                'message' => 'Article Empty'
+            ], Response::HTTP_NOT_FOUND);
+        }else {
+            return response()->json([
+                'data' => $articles->map(function ($article) {
+                    return [
+                        'title' => $article->title,
+                        'content' => $article->content,
+                        'published_date' => $article->published_date
+                    ];
+                }),
+                'message' => 'list Article',
+                'status' => Response::HTTP_OK
+            ], Response::HTTP_OK);
+        }
+
+    }
 }
